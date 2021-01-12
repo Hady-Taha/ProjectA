e551d1797d8f5cef3b5a19eae0398dbda5fa2ea2
root <www.abdallawalled06@gmail.com>
2021-01-12 14:14:48 -0500

    edit

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
+
+STUDENTGENDER = [('male','male'),('female','female')]
 # Create your models here.
 class StudentProfile(models.Model):
-
-
-
-
+        firstName=models.CharField(max_length=150,blank=True, null=True)
+        lastName=models.CharField(max_length=150,blank=True, null=True)
+        studentId=models.IntegerField(blank=True, null=True)
+        level=models.CharField(max_length=30, blank=True , null=True)
+        department= models.CharField(max_length=30,blank=True, null=True)
+        gender = models.CharField(max_length=20,)
+        image = models.ImageField(upload_to='studentImages', default='studentImages/user.png')
+        created = models.DateTimeField(auto_now_add=True)
+        ver = models.BooleanField(default=False)
 
 
 
