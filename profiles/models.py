e34d3a28fe8b2db9293f2e54328c4e5da95552ee
Hady-Taha <didotaha48@gmail.com>
2021-01-12 18:12:02 +0200

    todolist

--- profiles/models.py
+++ profiles/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
+    """
+    TODO
+    1- import model User from auth model .
+    2- add fields .
+    3- add functions .
+    
+    """
+    
     pass
