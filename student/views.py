93cfa654299720e11790cca470b5235dcd97acc3
Hady-Taha <didotaha48@gmail.com>
2021-01-14 23:55:04 +0200

    edit9

--- student/views.py
+++ student/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .forms import AddNewStudent
 # Create your views here.
 
 def student(request):
+    form=AddNewStudent()
     context = {
-        'title':'registeration',
+        'title':'registration',
+        'form':form,
     }
     return render(request, 'student/student.html', context)
