f3a72342e0b3c5e5928ff59d0e43a5434a75efe0
Abdalla-Walled <www.abdallawalled06@gmail.com>
2021-01-14 12:24:48 -0800

    edit

--- student/views.py
+++ student/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def student(request):
+    context = {
+        'title':'registeration',
+    }
+    return render(request, 'student/student.html', context)
