2068ad9df7a06dd5bc4301b4abed792f07304e9c
Hady-Taha <didotaha48@gmail.com>
2021-01-16 22:04:56 +0200

    edit5

--- student/views.py
+++ student/views.py
@@ -8,10 +8,15 @@ def student(request):
         form = AddNewStudent(request.POST or None, request.FILES or None)
         if form.is_valid():
             form.save()
-            pass
+            
         pass
     context = {
         'title':'registration',
         'form':form,
     }
     return render(request, 'student/student.html', context)
+
+
+def attendance(request):
+    
+    return render(request, 'student/attendance.html')
