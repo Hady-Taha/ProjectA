cb15837b2b5bf774462aae50816d4f12ef43c680
Hady-Taha <didotaha48@gmail.com>
2021-01-18 11:19:28 +0200

    fix9

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -1,47 +1,85 @@
+/* Function to open the camera*/
 $(function () {
-    // Grab elements, create settings, etc.
+
+  $('#openPhoto').click(function (e) {
+    e.preventDefault();
+    openFun()
+  });
+
+
+  $('#videoOff').click(function (e) {
+    e.preventDefault();
+    vidOff()
+  });
+
+  $('#photo').click(function (e) {
+    e.preventDefault();
+    photoFun()
+    
+    
+    
+    console.log(photoFun())
+  
+  });
+
+
+function openFun() {
+
     var video = document.getElementById('video');
-    // Elements for taking the snapshot
+
+    // Get access to the camera!
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(function (stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+      });
+    }
+
+  }
+
+function vidOff() {
+    const video = document.querySelector('video');
+
+    // A video's MediaStream object is available through its srcObject attribute
+    const mediaStream = video.srcObject;
+
+    // Through the MediaStream, you can get the MediaStreamTracks with getTracks():
+    const tracks = mediaStream.getTracks();
+
+    // Tracks are returned as an array, so if you know you only have one, you can stop it with: 
+    tracks[0].stop();
+
+    // Or stop all like so:
+  tracks.forEach(track => track.stop())
+}
+
+  var photoFun = function () {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
+    var video = document.getElementById('video');
 
-    document.getElementById('start').addEventListener("click", function () {
-        // Get access to the camera!
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({
-                video: true
-            }).then(function (stream) {
-                //video.src = window.URL.createObjectURL(stream);
-                video.srcObject = stream;
-                video.play();
-            });
-        }
+    context.drawImage(video, 0, 0, 600, 480);
+    var dataURL = canvas.toDataURL();
 
-        $('#snap').fadeIn();
 
-        // Trigger photo take
-        document.getElementById("snap").addEventListener("click", function () {
-            context.drawImage(video, 0, 0, 640, 480);
+    return dataURL;
+  }
 
-            var dataURL = canvas.toDataURL();
 
-            $("#use_image").click(function () {
-                let $form = $(".image_submit_form");
-                let form_data = new FormData($form[0]);
 
-                $.ajax({
-                    url: $form.attr('action'),
-                    type: $form.attr('method'),
-                    dataType: 'json',
-                    data: JSON.stringify(dataURL),
-                    data: {
-                        imageBase64: dataURL
-                    }
-                }).done(function () {
-                    console.log('sent');
-                });
-            });
-        });
+});
 
-    });
-})
+
+
+
+
+
+
+
+
+
+/*<!--#############################################################################################################-->*/
