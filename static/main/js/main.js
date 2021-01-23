b44ac38c25299abd2f75d4389019d1aeade6be87
Hady-Taha <didotaha48@gmail.com>
2021-01-23 20:00:32 +0200

    add

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -1,97 +1,95 @@
 /* Function to open the camera*/
-$(function () {
-
-  $('#openPhoto').click(function (e) {
-    e.preventDefault();
-    openFun()
-  });
-
-
-  $('#videoOff').click(function (e) {
-    e.preventDefault();
-    vidOff()
-  });
-
-  $('.done').submit(function (e) {
-    e.preventDefault();
-    let token = $('input[name=csrfmiddlewaretoken]').val()
-    let url = $(this).data('url')
-
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: { csrfmiddlewaretoken: token , data:photoFun() },
-      success: function (response) {
-        alert('devo')
-      }
-    });
-
-    console.log(serialize);
-
-  });
 
 
 
 
 
+function openFun() {
+  var video = document.getElementById("video");
 
-  
-  function openFun() {
-
-    var video = document.getElementById('video');
-
-    // Get access to the camera!
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      // Not adding `{ audio: true }` since we only want video now
-      navigator.mediaDevices.getUserMedia({
-        video: true
-      }).then(function (stream) {
+  // Get access to the camera!
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+      })
+      .then(function (stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
         video.play();
       });
-    }
-
   }
+}
 
-  function vidOff() {
-    const video = document.querySelector('video');
+/*<!--#############################################################################################################-->*/
 
-    // A video's MediaStream object is available through its srcObject attribute
-    const mediaStream = video.srcObject;
+/* Function to close the camera*/
 
-    // Through the MediaStream, you can get the MediaStreamTracks with getTracks():
-    const tracks = mediaStream.getTracks();
+function vidOff() {
+  const video = document.querySelector("video");
 
-    // Tracks are returned as an array, so if you know you only have one, you can stop it with: 
-    tracks[0].stop();
+  // A video's MediaStream object is available through its srcObject attribute
+  const mediaStream = video.srcObject;
 
-    // Or stop all like so:
-    tracks.forEach(track => track.stop())
-  }
+  // Through the MediaStream, you can get the MediaStreamTracks with getTracks():
+  const tracks = mediaStream.getTracks();
+
+  // Tracks are returned as an array, so if you know you only have one, you can stop it with:
+  tracks[0].stop();
+
+  // Or stop all like so:
+  tracks.forEach((track) => track.stop());
+}
+
+/*<!--#############################################################################################################-->*/
+
+/* Function to take photo and matching it */
+
+function photoFun() {
+  // Elements for taking the snapshot
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  var video = document.getElementById("video");
+  // Trigger photo take
+  context.drawImage(video, 0, 0, 600, 600);
+}
+
+//ابلكيشن الالكل
+function covertData() {
+  var canvas = document.getElementById("canvas");
+  var dataurl = canvas.toDataURL();
+  
+  return dataurl
+}
+
+/*<!--#############################################################################################################-->*/
+
+/* Attendance registration */
 
 
-  var photoFun = function () {
+$(function () {
 
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    var video = document.getElementById('video');
-    context.drawImage(video, 0, 0, 600, 480);
-    var dataURL = canvas.toDataURL();
-    return dataURL;
+  $(".sendToServer").submit(function (e) {
+    e.preventDefault();
+    let url = $(this).data('url')
+    let token = $("input[name=csrfmiddlewaretoken]").val();
 
-  }
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: {
+        csrfmiddlewaretoken: token,
+        data: covertData(),
+      },
+      success: function (response) {
+        alert("devo");
+      },
+    });
+  
+  
+  });
 
-
+  //csrfmiddlewaretoken‏
 });
-
-
-
-
-
-
-
-
-
-
 /*<!--#############################################################################################################-->*/
